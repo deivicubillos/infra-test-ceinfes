108c7cae1d0a48e5794650ddca1ddec5a0cddd0f
Deivi Cubillos <deivicubillos@gmail.com>
2026-04-10 18:43:26 -0500

    Bonus: Implementación de logs persistentes

--- app/app.py
+++ app/app.py
@@ -1,24 +1,29 @@
-import logging
-from logging.handlers import RotatingFileHandler
-
-# Configuración de Logs Claros
-logging.basicConfig(
-    level=logging.INFO,
-    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    handlers=[
-        RotatingFileHandler("logs/app.log", maxBytes=10000, backupCount=3),
-        logging.StreamHandler() # Esto permite verlos también con 'docker logs'
-    ]
-)
-
-from flask import Flask
 import os
+import logging
+from flask import Flask
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return f"Hello from {os.getenv('ENV', 'dev')}!"
+# --- CONFIGURACIÓN DE LOGS PROFESIONALES ---
+if not os.path.exists('logs'):
+    os.mkdir('logs')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('App iniciando...')
+# ------------------------------------------
+
+@app.route('/')
+def hello():
+    env = os.getenv('ENV', 'dev')
+    app.logger.info(f'Petición recibida en entorno: {env}') # Registro de cada visita
+    return f"Hello from {env}!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
